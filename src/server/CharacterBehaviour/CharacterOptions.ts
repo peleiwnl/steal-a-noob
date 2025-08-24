a7daa326f031243f4136744297bd282878f21b97
peleiwnl <moomayle@hotmail.co.uk>
2025-08-24 20:37:56 +0100

    Initial Commit

---
+++ src/server/CharacterBehaviour/CharacterOptions.ts
@@ -0,0 +1,8 @@
+import { CharacterType } from "./CharacterType";
+
+interface CharacterOptions {
+	type: CharacterType;
+	size?: Vector3;
+	speed?: number;
+	color?: Color3;
+}
---
+++ src/server/CharacterBehaviour/CharacterType.ts
@@ -0,0 +1,5 @@
+export enum CharacterType {
+	Noob = "Noob",
+	MegaNoob = "MegaNoob",
+	NewerNoob = "NewerNoob",
+}
